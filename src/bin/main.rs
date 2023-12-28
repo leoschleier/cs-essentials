5c34cfc2af83147cdcb0c2a6eab6460af0ce5807
Leo Schleier <43878374+leoschleier@users.noreply.github.com>
2023-12-28 23:10:21 +0100

    Finish hash table implementation

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,3 +1,26 @@
-fn main(){
-    println!("Executing package binary.");
+use log::debug;
+
+use cs_essentials::hash_table::HashTable;
+
+fn main() {
+    env_logger::init();
+    debug!("Executing binary.");
+
+    let mut my_hash_table = HashTable::<i32>::new();
+
+    my_hash_table.add("my_key", 69);
+    my_hash_table.add("my_second_key", 69_i32.pow(2));
+
+    let my_fav_numb = my_hash_table.get("my_key");
+    let my_second_fav_numb = my_hash_table.get("my_second_key");
+
+    match my_fav_numb {
+        Some(numb) => println!("My favorite number: {numb}"),
+        _ => println!("Number not found."),
+    }
+
+    match my_second_fav_numb {
+        Some(numb) => println!("My second favorite number: {numb}"),
+        _ => println!("Number not found."),
+    }
 }
---
+++ src/hash_table.rs
@@ -0,0 +1,135 @@
+use log::debug;
+
+#[derive(PartialEq)]
+struct Node<T> {
+    value: T,
+    next: Box<LinkedList<T>>,
+}
+
+#[derive(PartialEq)]
+struct KeyValue<T> {
+    key: Box<str>,
+    value: T,
+}
+
+#[derive(PartialEq)]
+enum LinkedList<T> {
+    Null,
+    Node(Node<T>),
+}
+
+const TABLE_LEN: usize = 10;
+const TABLE_LEN_U64: u64 = TABLE_LEN as u64;
+
+pub struct HashTable<T> {
+    arr: [LinkedList<KeyValue<T>>; TABLE_LEN],
+}
+
+impl<T: std::cmp::PartialEq> HashTable<T> {
+    /// Create a new `HashTable`.
+    pub fn new() -> Self {
+        HashTable {
+            arr: core::array::from_fn::<LinkedList<KeyValue<T>>, TABLE_LEN, _>(
+                |_| LinkedList::Null,
+            ),
+        }
+    }
+
+    /// Add a key-value pair to the `HahTable`.
+    pub fn add(&mut self, key: &str, value: T) {
+        let index = HashTable::<T>::get_index(key);
+        debug!("Add at index {index}.");
+
+        let mut linked_list = &mut self.arr[index];
+
+        match linked_list {
+            // If first node is Null --> add first node with value at index and return
+            LinkedList::Null => {
+                *linked_list = LinkedList::Node(
+                    HashTable::<T>::create_tail_node(key, value),
+                );
+                return;
+            }
+            _ => (),
+        }
+
+        // Iterate over nodes in LinkedList
+        while let LinkedList::Node(ref mut node) = linked_list {
+            if *node.value.key == *key {
+                // If key == key --> replace value and retrun
+                debug!("Matching key found. Replace existing");
+                node.value.value = value;
+                break;
+            }
+            if *node.next == LinkedList::Null {
+                // If at the end of LinkedList --> Append new node
+                debug!("Reached end. Add new node.");
+                node.next = Box::new(LinkedList::Node(
+                    HashTable::create_tail_node(key, value),
+                ));
+                break;
+            }
+
+            // Continue
+            debug!("Continue.");
+            linked_list = &mut *node.next;
+        }
+    }
+
+    /// Get the value for a given key.
+    pub fn get(&self, key: &str) -> Option<&T> {
+        let index = HashTable::<T>::get_index(key);
+        debug!("Look up at index {index}.");
+
+        let mut linked_list = &self.arr[index];
+
+        //Iterate over nodes in LinkedList
+        while let LinkedList::Node(node) = linked_list {
+            debug!("Current key: {:?}.", node.value.key);
+            if *node.value.key == *key {
+                // If key == key --> return value
+                debug!("Matching key found.");
+                return Some(&node.value.value);
+            }
+
+            // Continue
+            debug!("Continue.");
+            linked_list = &*node.next;
+        }
+
+        // If key not contained in `HashTable` --> return None
+        None
+    }
+
+    fn create_tail_node(key: &str, value: T) -> Node<KeyValue<T>> {
+        let node = Node {
+            value: KeyValue {
+                key: key.into(),
+                value,
+            },
+            next: Box::new(LinkedList::Null),
+        };
+
+        node
+    }
+
+    /// Compute index for a string in the internal array by using a hash function.
+    fn get_index(s: &str) -> usize {
+        (fnv_1(s.as_bytes()) % TABLE_LEN_U64) as usize
+    }
+}
+
+const FNV_OFFSET_BASIS: u64 = 14695981039346656037;
+const FNV_PRIME: u64 = 1099511628211;
+
+/// Compute the hash of a bytes array by using the Fowler-Noll-Vo hash function.
+/// See: https://en.wikipedia.org/wiki/Fowler–Noll–Vo_hash_function
+fn fnv_1(bytes: &[u8]) -> u64 {
+    let mut hash = FNV_OFFSET_BASIS;
+    for byte in bytes {
+        hash = (hash as u128 * FNV_PRIME as u128) as u64;
+        hash = hash ^ (*byte as u64);
+    }
+
+    hash
+}
