9050a366c1ff35813b461df7e9d959ac7dcebae4
Leo Schleier <43878374+leoschleier@users.noreply.github.com>
2024-06-05 22:02:33 +0200

    Extend stack

--- cs-python/cs_essentials/stacks_and_queues/stack.py
+++ cs-python/cs_essentials/stacks_and_queues/stack.py
@@ -8,6 +8,8 @@ class Node[T]:
     def __str__(self) -> str:
         return str(self.value)
 
+class EmptyStackException(Exception):
+    ...
 
 
 class Stack[T]:
@@ -27,30 +29,41 @@ class Stack[T]:
 
         return s + f"{node}]"
 
-    def add(self, value: T):
+    def push(self, value: T):
         new_node = Node(value)
         new_node.next = self._head
         self._head = new_node
 
-    def pop(self) -> Node[T] | None:
+    def pop(self) -> Node[T]:
         if self._head is None:
-            return None
+            raise EmptyStackException
 
         node = self._head
         self._head = node.next
 
         return node
+
+    def peek(self) -> Node[T]:
+        if self._head is None:
+            raise EmptyStackException
+
+        return self._head
+
+
+    def is_empty(self) -> bool:
+        return self._head is None
         
 
 if __name__ == "__main__":
     print("Test Stack") 
 
     stack = Stack[int]()
+    stack.peek()
     print(stack)
 
-    stack.add(3)
-    stack.add(1)
-    stack.add(5)
+    stack.push(3)
+    stack.push(1)
+    stack.push(5)
 
     print(stack)
     
